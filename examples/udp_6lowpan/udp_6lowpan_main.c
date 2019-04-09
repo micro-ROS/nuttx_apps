2eb83bc63f548806872db79c712e3941cedaeea3
Juan Flores <jfloresm92@gmail.com>
2019-04-09 18:16:48 +0200

    Adding autoconfiguration settings

--- examples/udp_6lowpan/udp_6lowpan_main.c
+++ examples/udp_6lowpan/udp_6lowpan_main.c
@@ -58,6 +58,7 @@
  ****************************************************************************/
 int udp_write();
 void udp_read();
+void lowpan_configuration();
 
 /****************************************************************************
  * Variables
@@ -79,22 +80,29 @@ uint16_t g_udpserver_ipv6[8] =
  * Main Functions
  ****************************************************************************/
 int udp_6lowpan_main(int argc, char *argv[]){
-  if(argc < 2){
-    //Mostrar el eror
-    printf("Bad ussage \r\n");
-    return 0;
+
+  printf("Do you want to execute the automatic WPAN configuration? (y/n)\n");
+  char aux=getchar();
+  if(aux=='y') lowpan_configuration();
+
+  while(1){
+    printf("\nAvailable commands\n -To send a package type: write \n -To receive a package type: read \n -To exit type: quit\n");
+    char buffer[256];
+    scanf("%255s",buffer);
+    if(strcmp(buffer, "write") == 0){
+      udp_write();
+    }
+    else if(strcmp(buffer, "read") == 0){
+      udp_read();
+    }
+    else if(strcmp(buffer, "quit") == 0){
+      return 0;
+    }
+    else{
+      printf("\nWrong command.\n -To send a package type: write \n -To receive a package type: read \n -To exit type: quit\n");
+    }
   }
 
-  if(strcmp(argv[1], "write") == 0){
-    udp_write();
-  }
-  else if(strcmp(argv[1], "read") == 0){
-    udp_read();
-  }
-  else{
-    printf("Error\r\n");
-    return 0;
-  }
 }
 
 int udp_write(){
@@ -153,7 +161,7 @@ int udp_write(){
   /* Bind the UDP socket to a IPv6 port */
 
   server.sin6_family     = AF_INET6;
-  server.sin6_port       = HTONS(8080);
+  server.sin6_port       = HTONS(atoi(port_origin));
   memset(server.sin6_addr.s6_addr, 0, sizeof(struct in6_addr));
   addrlen              = sizeof(struct sockaddr_in6);
 
@@ -166,7 +174,7 @@ int udp_write(){
 
   //Copy the server configuration
   server.sin6_family            = AF_INET6;
-  server.sin6_port              = HTONS(61616);
+  server.sin6_port              = HTONS(atoi(port_destination));
   memcpy(server.sin6_addr.s6_addr16, g_udpserver_ipv6, 8 * sizeof(uint16_t));
   addrlen                       = sizeof(struct sockaddr_in6);
 
@@ -175,9 +183,13 @@ int udp_write(){
     char buffer[256];
     printf("Introduce a message to send:\r\n");
     char_rec=scanf("%255s",buffer);
-    if(strcmp("quit",buffer)==0) return 0;
-    printf("Sending %i characters: %s \r\n",char_rec,buffer);
-    sendto(sockfd, buffer, 4, 0,
+    if(strcmp("quit",buffer)==0){
+      printf("Closing connection\n");
+      close(sockfd);
+      return;
+    }
+    printf("Sending %i characters: %s \n\n",strlen(buffer),buffer);
+    sendto(sockfd, buffer, strlen(buffer), 0,
                     (struct sockaddr*)&server, addrlen);
   }
 
@@ -189,5 +201,59 @@ void udp_read(){
 }
 
 void lowpan_configuration(){
+  char buffer[256];
+  int id=0;
+  printf("Starting WPAN configuration\n");
+  printf("Type C to be coordinator\nType N to be node\n\n\n");
+
+  scanf("%1s",buffer);
+  system("ifdown wpan0");
+  if(strcmp("c",buffer)==0){
+    //Set coordinator
+    system("i8sak wpan0 startpan cd:ab");
+    system("i8sak set chan 26");
+    sleep(1);
+    printf("Choose your ID (00 to FF)\n\n");
+    scanf("%s",buffer);
+    if(strlen(buffer)<2 ){
+      printf("Error ID must be between 00 and FF\n");
+      return 0;
+    }
+    system("i8sak set saddr 42:01");
+    sprintf(buffer,"i8sak set eaddr 00:fa:de:00:de:ad:be:%c%c",buffer[0],buffer[1]);
+    printf("Your hardware address is: %s\n\n",buffer);
+    system(buffer);
+    system("i8sak acceptassoc");
+
+  }
+  else if(strcmp("n",buffer)==0){
+    //Set node
+    system("i8sak wpan0");
+    system("i8sak set chan 26");
+    system("i8sak set panid cd:ab");
+    sleep(1);
+    printf("Choose your ID (00 to FF)\n\n");
+    scanf("%s",buffer);
+    if(strlen(buffer)<2 ){
+      printf("Error ID must be between 00 and FF\n");
+      return 0;
+    }
+
+    sprintf(buffer,"i8sak set eaddr 00:fa:de:00:de:ad:be:%c%c",buffer[0],buffer[1]);
+    system(buffer);
+    system("i8sak set ep_saddr 42:01");
+    system("i8sak set saddr 42:02");
+    printf("Your hardware address is: %s\n\n",buffer);
+    system("i8sak assoc");
+  }
+  else{
+    printf("Wrong profile\r\n");
+  }
+  system("ifup wpan0");
+
+  printf("Mounting proc file system\n");
+  system("mount -t procfs /proc");
+  system("cat proc/net/wpan0");
+  printf("\n\n");
 
 }

53e801e3ba61f3646d8ebe2b760319fdd6f50251
Ingo Lütkebohle <ingo.luetkebohle@de.bosch.com>
2019-05-15 13:01:52 +0200

    Drive base (#6)
    * Start of Kobuki lib
    * make location of workspace configurable
    * use TOPDIR instead of hard-coded NuttX location
    * specify correct target for compiler detection
    * generate arm_toolchain.cmake from variables, to correctly pass it on to dependent builds such as libpoco
    * replace standard romfs with kobuki startup (in drive_base config)

---
+++ examples/kobuki/kobuki_protocol.cxx
@@ -0,0 +1,473 @@
+// Copyright (c) 2015, Lab-RoCoCo
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+
+// * Neither the name of thin_drivers nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+#include "kobuki_protocol.h"
+#include <stdexcept>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+/************************************************************/
+
+SubPayload::SubPayload(uint8_t header_, uint8_t length_) {
+  _header=header_;
+  _length=length_;
+}
+
+int SubPayload::parse(const unsigned char*&buffer) {
+  throw std::runtime_error("not implemented");
+}
+int SubPayload::write(unsigned char*&buffer) {
+  throw std::runtime_error("not implemented");
+}
+  
+SubPayload::~SubPayload(){}
+
+
+/************************************************************/
+
+BasicSensorDataPayload::BasicSensorDataPayload(): SubPayload(BasicSensorDataPayload_HEADER,15){}
+int BasicSensorDataPayload::parse(const unsigned char*&buffer) {
+  timestamp = parseShort(buffer);
+  bumper = parseChar(buffer);
+  wheel_drop = parseChar(buffer);
+  cliff = parseChar(buffer);
+  left_encoder = parseShort(buffer);
+  right_encoder = parseShort(buffer);
+  left_pwm = parseChar(buffer);
+  right_pwm = parseChar(buffer);
+  button = parseChar(buffer);
+  charger = parseChar(buffer);
+  battery = parseChar(buffer);
+  overcurrent_flags = parseChar(buffer);
+  return _length;
+}
+
+/************************************************************/
+DockingIRPayload::DockingIRPayload(): SubPayload(DockingIRPayload_HEADER,3){}
+
+int DockingIRPayload::parse(const unsigned char*&buffer) {
+  right_signal = parseChar(buffer);
+  center_signal = parseChar(buffer);
+  left_signal = parseChar(buffer);
+  return 3;
+}
+
+/************************************************************/
+InertialSensorDataPayload::InertialSensorDataPayload(): SubPayload(InertialSensorDataPayload_HEADER,7){}
+
+int InertialSensorDataPayload::parse(const unsigned char*&buffer) {
+  angle = parseShort(buffer);
+  rate =parseShort(buffer);
+  for(int i = 0 ; i<3; i++)
+    unused[i]  = parseChar(buffer);
+  return 7;
+}
+
+/************************************************************/
+
+CliffSensorDataPayload::CliffSensorDataPayload(): SubPayload(CliffSensorDataPayload_HEADER,6){}
+int CliffSensorDataPayload::parse(const unsigned char*&buffer) {
+  right_signal = parseShort(buffer);
+  center_signal =parseShort(buffer);
+  left_signal = parseShort(buffer);
+  return 6;
+}
+
+/************************************************************/
+
+CurrentPayload::CurrentPayload(): SubPayload(CurrentPayload_HEADER,2){}
+
+int CurrentPayload::parse(const unsigned char*&buffer) {
+  right_motor = parseChar(buffer);
+  left_motor = parseChar(buffer);
+  return 2;
+}
+
+
+/************************************************************/
+
+HardwareVersionPayload::HardwareVersionPayload(): SubPayload(HardwareVersionPayload_HEADER,4){}
+
+int HardwareVersionPayload::parse(const unsigned char*&buffer) {
+  patch = parseChar(buffer);
+  major = parseChar(buffer);
+  minor = parseChar(buffer);
+  unised = parseChar(buffer);
+  return 4;
+}
+
+
+/************************************************************/
+
+FirmwareVersionPayload::FirmwareVersionPayload(): SubPayload(FirmwareVersionPayload_HEADER,4){}
+
+int FirmwareVersionPayload::parse(const unsigned char*&buffer) {
+  patch = parseChar(buffer);
+  major = parseChar(buffer);
+  minor = parseChar(buffer);
+  unised = parseChar(buffer);
+  return 4;
+}
+
+
+/************************************************************/
+
+GyroPayload::GyroPayload(): SubPayload(GyroPayload_HEADER,-1){}
+
+int GyroPayload::parse(const unsigned char*&buffer) {
+  frame_id = parseChar(buffer);
+  subdata_length = parseChar(buffer);
+  size_t s = subdata_length/3;
+  num_gyro_data = s;
+  for (size_t i=0; i<s; i++){
+    gyro_datas[i].x=parseShort(buffer);
+    gyro_datas[i].y=parseShort(buffer);
+    gyro_datas[i].z=parseShort(buffer);
+  }
+  _length = 2+6*s;
+  return 2+6*s;
+}
+
+
+/************************************************************/
+
+GPIOPayload::GPIOPayload(): SubPayload(GPIOPayload_HEADER,16){}
+
+int GPIOPayload::parse(const unsigned char*&buffer) {
+  digital_input = parseShort(buffer);
+  for(int i = 0; i<4; i++){
+    analog_input[i] = parseShort(buffer);
+  }
+  for(int i = 0; i<3; i++){
+    unused[i] = parseShort(buffer);
+  }
+  return 16;
+}
+
+
+/************************************************************/
+
+UUIDPayload::UUIDPayload(): SubPayload(UUIDPayload_HEADER,12){}
+
+int UUIDPayload::parse(const unsigned char*&buffer) {
+  for(int i = 0; i<3; i++){
+    udid[i] = parseInt(buffer);
+  }
+  return 12;
+}
+
+
+/************************************************************/
+ControllerInfoPayload::ControllerInfoPayload(): SubPayload(ControllerInfoPayload_HEADER,7){}
+
+int ControllerInfoPayload::parse(const unsigned char*&buffer) {
+  type=parseChar(buffer);
+  P=parseInt(buffer);
+  I=parseInt(buffer);
+  D=parseInt(buffer);
+  return 7;
+}
+
+
+/************************************************************/
+
+BaseControlPayload::BaseControlPayload(): SubPayload(BaseControlPayload_HEADER,4){}
+
+int BaseControlPayload::write(unsigned char*&buffer) {
+  int k = 0;
+  k+=writeShort(buffer, speed);
+  k+=writeShort(buffer, radius);
+  return k;
+}
+
+/************************************************************/
+SoundPayload::SoundPayload(): SubPayload(SoundPayload_HEADER,3){}
+
+int SoundPayload::write(unsigned char*&buffer) {
+  int k = 0;
+  k+=writeShort(buffer, note);
+  k+=writeChar(buffer, duration);
+  return k;
+}
+
+/************************************************************/
+
+SoundSequencePayload::SoundSequencePayload(): SubPayload(SoundSequencePayload_HEADER,1){}
+
+int SoundSequencePayload::write(unsigned char*&buffer) {
+  int k = 0;
+  k+=writeShort(buffer, sequence);
+  return k;
+}
+
+/************************************************************/
+
+RequestExtraPayload::RequestExtraPayload(): SubPayload(RequestExtraPayload_HEADER,1){}
+
+int RequestExtraPayload::write(unsigned char*&buffer) {
+    int k = 0;
+    k+=writeShort(buffer, request_flags);
+    return k;
+  }
+
+
+/************************************************************/
+
+GPIOOutputPayload::GPIOOutputPayload(): SubPayload(GPIOOutputPayload_HEADER,2){}
+
+int GPIOOutputPayload::write(unsigned char*&buffer) {
+  int k = 0;
+  k+=writeShort(buffer, digital_output_flags);
+  return k;
+}
+
+
+/************************************************************/
+
+SetControllerGain::SetControllerGain(): SubPayload(SetControllerGain_HEADER,2){}
+
+int SetControllerGain::write(unsigned char*&buffer) {
+  int k = 0;
+  k+=writeChar(buffer, type);
+  k+=writeInt(buffer, P);
+  k+=writeInt(buffer, I);
+  k+=writeInt(buffer, D);
+  return k;
+}
+
+
+/************************************************************/
+
+GetControllerGain::GetControllerGain(): SubPayload(GetControllerGain_HEADER,1){}
+int GetControllerGain::write(unsigned char*&buffer) {
+  int k = 0;
+  k+=writeChar(buffer, unused);
+  return k;
+}
+
+
+/************************************************************/
+
+Packet::Packet(){ length=0;}
+
+
+Packet::~Packet() {
+  clear();
+}
+
+
+void Packet::clear() {
+  for (size_t i = 0; i<_payloads.size(); i++){
+    delete _payloads[i];
+  }
+  _payloads.clear();
+}
+
+int Packet::write(unsigned char* buffer){
+  unsigned char* b = buffer;
+  int k = 0;
+  k+=writeChar(b,0xAA);
+  k+=writeChar(b,0x55);
+  k+=writeChar(b,0x00);
+  for (size_t i=0; i<_payloads.size(); i++) {
+    SubPayload* p=_payloads[i];
+    k+=writeChar(b,p->header());
+    k+=writeChar(b,p->length());
+    k+=p->write(b);
+  }
+  if (k>255)
+    return -1;
+  buffer[2] = k-1;
+  unsigned char cs=0;
+  for (uint8_t i = 2; i<k; i++){
+    cs^=buffer[i];
+  }
+  k+=writeChar(b,cs);
+  
+  int size = b-buffer;
+  return size;
+}
+
+/************************************************************/
+PacketSyncFinder::PacketSyncFinder() : _length(0), _state(Unsynced), _packet_ready(false){
+}
+
+void PacketSyncFinder::putChar(unsigned char c) {
+  _packet_ready = false;
+  switch(_state){
+  case Unsynced:
+    if (c==0xAA){
+      _state = Sync1;
+      //cerr << "sync1" << endl;
+    }
+    break;
+  case Sync1:
+    if (c==0x55){
+      _state = Length;
+      //cerr << "sync2" << endl;
+    }
+    break;
+  case Length:
+    _length=c;
+    _payload_buf_idx = 0;
+    _state=Payload;
+    //cerr << "length: " << (int) length << endl;;
+    break;
+  case Payload:
+    //cerr << ".";
+    _payload_buf[_payload_buf_idx]=c;
+    _payload_buf_idx++;
+    if (_payload_buf_idx==_length)
+      _state = Checksum;
+    break;
+  case Checksum:
+    {
+    unsigned char cs=0;
+    cs^=_length;
+    cs^=c;
+    for (unsigned int i = 0; i < _length; i++) {
+      cs ^=  _payload_buf[i];
+    }
+    if (!cs) {
+      //cerr << "packet ready" << endl;
+      _packet_ready=true;
+    }
+    _state = Unsynced;
+    }
+    break;
+  }
+  
+}
+
+
+/************************************************************/
+
+
+// these are only in the c file
+struct BasePayloadCreator {
+  virtual SubPayload* create() =0;
+  virtual ~BasePayloadCreator(){};
+  uint8_t header;
+};
+
+
+template <typename T>
+struct PayloadCreator: public BasePayloadCreator{
+  PayloadCreator(){
+    T t;
+    header = t.header();
+  }
+  virtual SubPayload* create() {
+    return new T();
+  }
+};
+
+
+PacketParser::PacketParser(){
+  printf("pp ctor\n");
+  _creators.resize(30,0);
+  BasePayloadCreator* c;
+  c= new PayloadCreator<BasicSensorDataPayload>;
+  _creators[c->header] = c;
+  
+  //c= new PayloadCreator<DockingIRPayload>;
+  //_creators[c->header] = c;
+
+  c= new PayloadCreator<InertialSensorDataPayload>;
+  _creators[c->header] = c;
+/*
+  /c= new PayloadCreator<CliffSensorDataPayload>;
+  _creators[c->header] = c;
+
+  c= new PayloadCreator<CurrentPayload>;
+  _creators[c->header] = c;
+
+  c= new PayloadCreator<HardwareVersionPayload>;
+  _creators[c->header] = c;
+
+  c= new PayloadCreator<FirmwareVersionPayload>;
+  _creators[c->header] = c;
+
+  c= new PayloadCreator<GyroPayload>;
+  _creators[c->header] = c;
+*/
+  /*
+  c= new PayloadCreator<GPIOPayload>;
+  _creators[c->header] = c;
+  */
+
+ /* c= new PayloadCreator<UUIDPayload>;
+  _creators[c->header] = c;
+
+  c= new PayloadCreator<ControllerInfoPayload>;
+  _creators[c->header] = c;*/
+}
+  
+SubPayload* PacketParser::createPayload(uint8_t header) {
+  if(header >= 30)
+	  return NULL;
+  BasePayloadCreator *c = _creators[header];
+  if(c == NULL) {
+	 return NULL;
+  } else {
+	  return c->create();
+  }
+}
+
+Packet* PacketParser::parseBuffer(const unsigned char*& buffer, uint8_t length){
+  int i = 0;
+  if(length == 0)
+	  return NULL;
+  Packet* p = new Packet();
+  p->length = length;
+  while (i<length) {
+    uint8_t type = parseChar(buffer);
+    i++;
+    SubPayload* bp = createPayload(type);
+    if ( ! bp) {
+      break;
+    }
+    uint8_t l = parseChar(buffer);
+    i++;
+    i+=bp->parse(buffer);
+    p->_payloads.push_back(bp);
+    if (l!=bp->length()){
+      throw std::runtime_error("packet length mismatch");
+    }
+  }
+  return p;
+}
+
+
+
+
---
+++ examples/kobuki/thin_kobuki_node.cxx
@@ -0,0 +1,240 @@
+// Copyright (c) 2018, Robert Bosch GmbH
+// Copyright (c) 2015, Lab-RoCoCo
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+
+// * Neither the name of thin_drivers nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <nuttx/config.h>
+#include <rclc/rclc.h>
+#include <std_msgs/msg/int32.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <nuttx/init.h>
+#include "platform/cxxinitialize.h"
+
+
+//#include <ros/ros.h>
+//#include <ros/callback_queue.h>
+
+// ROS message types
+#include <geometry_msgs/msg/pose_stamped.h>
+#include <geometry_msgs/msg/vector3.h>
+#include <nav_msgs/msg/odometry.h>
+#include <sensor_msgs/msg/imu.h>
+#include <geometry_msgs/msg/twist.h>
+#include <geometry_msgs/msg/twist_stamped.h>
+
+// include message dependencies
+// for time ??? #include "builtin_interfaces/msg/time__struct.h"
+
+// strings in imu message : frame_id
+#include "rosidl_generator_c/string.h"
+
+#include "kobuki_robot.h"
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+//#include <iostream>
+//#include <tf/transform_datatypes.h>
+
+using namespace std;
+
+void kobuki_on_message(const void* msgin)
+{
+    const std_msgs__msg__Int32* msg = (const std_msgs__msg__Int32*)msgin;
+    printf("Subscriber: I heard: [%i]\n", msg->data);
+}
+
+// Stop motion if we do not receive a command without this timeout
+//const ros::Duration COMMAND_TIMEOUT(0.3);
+
+
+volatile float tv = 0, rv = 0;
+int numberMsgCmdVel = 0;
+
+//ros::Time last_update;
+uros_time_t last_update;
+
+struct mallinfo mi;
+static void
+display_mallinfo(void)
+{
+
+#ifdef CONFIG_CAN_PASS_STRUCTS
+  mi = mallinfo();
+#else
+  mallinfo(&mi);
+#endif
+
+  printf("Total non-mmapped bytes (arena):       %d\n", mi.arena);
+  printf("# of free chunks (ordblks):            %d\n", mi.ordblks);
+  printf("Size of largest free chunk (mxordblk)  %d\n", mi.mxordblk);
+  printf("Total allocated space (uordblks):      %d\n", mi.uordblks);
+  printf("Total free space (fordblks):           %d\n", mi.fordblks);
+}
+
+
+KobukiRobot *r;
+
+void commandVelCallback(const void * msgin){ //TwistConstPtr
+  const geometry_msgs__msg__Twist * twist = (const geometry_msgs__msg__Twist *)msgin;
+  numberMsgCmdVel++;
+  //printf("cmd_vel received(#%d)\n", numberMsgCmdVel);
+
+  if ( twist != NULL ) {
+    tv = (float)twist->linear.x;
+    rv = (float)twist->angular.z;
+    
+    r->setSpeed(tv, rv);
+    r->sendControls();
+
+    //printf("CommandVelCallback twist received (int) tv=%d rv=%d \n",(int) (twist->linear.x), (int)(twist->angular.z));
+
+    //debug output float CONFIG_LIBC_FLOATINGPOINT=y
+    //printf("cmd_vel (#%d): tv=%f rv=%lf \n", numberMsgCmdVel, (double) (twist->linear.x), (double) (twist->angular.z));
+    } else {
+    printf("Error in callback commandVelCallback Twist message expected!\n");
+  }
+  display_mallinfo();
+}
+
+
+extern "C"
+{
+#if defined(BUILD_MODULE)
+int main(int argc, char *argv[])
+#else
+int kobuki_main(int argc, char* argv[]) // name must match '$APPNAME_main' in Makefile (there is also a dependency on CONFIG_UROS_EXAMPLES_KOBUKI_PROGNAME)
+#endif
+  {
+    display_mallinfo();
+try {
+    KobukiRobot robot;
+    r = &robot;
+    int result = 0;
+    rclc_init(argc, argv);
+
+    printf("Turtlebot2 embedded kobuki driver\n");
+    rclc_node_t* node = NULL;
+    if (node = rclc_create_node("free_kobuki_node", ""))
+    {
+	//defines publishers and subscribers
+	rclc_publisher_t  * pub_odom                = NULL;
+	rclc_publisher_t  * pub_imu                 = NULL;
+	rclc_subscription_t * sub_cmd_vel             = NULL;
+
+	const char* pose_topic = "robot_pose";
+	if (pub_odom = rclc_create_publisher(node, RCLC_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Vector3), pose_topic, 1)){
+          printf("Created publisher: %s\n", pose_topic);
+        } else {
+          printf("Failed to create publisher: %s.\n", pose_topic);
+	  return -1;
+        }
+
+        if(sub_cmd_vel = rclc_create_subscription(node, RCLC_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+                                                        "cmd_vel", commandVelCallback, 10, false))
+        {
+	  printf("Created subscriber: cmd_vel.\n");
+        } else {
+	  printf("Failed to create subscriber: cmd_vel.\n");
+	  return -1;
+	}
+
+	// connections are properly setup
+	//TODO replace std::string with char var[256]
+	std::string serial_device;
+	std::string command_vel_topic;
+
+	
+   	serial_device     = "/dev/ttyS1";
+ 	command_vel_topic = "cmd_vel";
+
+        robot.connect(serial_device);
+
+	//ros::Rate r(100);
+	//TODO set rate
+	
+	geometry_msgs__msg__Vector3 pose;
+	geometry_msgs__msg__Vector3__init(&pose);
+
+  	int packet_count = 0;
+	float64 delta    = 0.0;
+	int count = 0;
+
+        display_mallinfo();
+  	while( true ){ // ros::ok() did not work on Olimex with micro-ROS
+		count++;
+	      uros_time_t timestamp;
+	      robot.receiveData(timestamp);
+
+	      if (packet_count < robot.packetCount()) {
+      	        packet_count = robot.packetCount();
+
+	        // send the odometry
+      	        float x,y,theta, vx, vtheta;
+      	        robot.getOdometry(x,y,theta,vx,vtheta);
+		/*if((count % 10) == 0) {
+	          printf("x=%f, y=%f, theta=%f, vx=%f, vtheta=%f\n",(double) x, (double)y, (double)theta, (double)vx, vtheta);
+		}*/
+	        pose.x = x;
+		pose.y = y;
+		pose.z = theta; // HACK!
+      	
+		if((count % 5) == 0) {	
+	          rclc_publish( pub_odom, (const void *) &pose);
+		}
+	
+      	        // imu data
+      	        //float heading;
+      	        //robot.getImu(heading, vtheta);
+	        //imu.header.seq = seq; //header.seq does not exist
+      	        //imu.header.stamp = timestamp;
+      	        //imu.orientation = tf::createQuaternionMsgFromRollPitchYaw(0.0, 0.0, heading);
+      	        //imu.angular_velocity.z = vtheta;
+      	      	
+	        //rclc_publish( pub_imu, (const void *) &imu); 
+	        //printf("Sending imu\n");
+	        // debug - sending a twist message - because odom and imu data produce error messages in nsh shell as well as in ros2-environment
+	        //rclc_publish ( pub_twist, (const void *) & msg_twist);
+	        //printf("Sending kobuki_twist\n");
+	     }
+
+	//spin once
+	rclc_spin_node_once(node, 10);
+        }        
+   }
+} catch(const std::exception& ex) {
+	printf("%s\n", ex.what());
+} catch(...) {
+	printf("Caught unknown error");
+}
+   return 0;
+    
+  } // main
+
+} // extern "C"
+

777587e991a9f230a8f3851ce62a2dfa33b1aa79
Juan Flores <jfloresm92@gmail.com>
2019-06-19 11:28:37 +0200

    Adding a stable version of VL53L1X demo app

--- examples/vl53l1x/vl53l1x_main.c
+++ examples/vl53l1x/vl53l1x_main.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * examples/hello/hello_main.c
+ * examples/vl53l1x/vl53l1x_main.c
  *
  *   Copyright (C) 2019 Acutronics Robotics. All rights reserved.
  *   Author: Acutronics Robotics (Juan Flores Muñoz) <juan@erlerobotics.com>
@@ -42,11 +42,17 @@
 #include <nuttx/sensors/vl53l1x.h>
 #include <fcntl.h>
 #include <nuttx/sensors/ioctl.h>
+#include <stdlib.h>     /* atoi */
 
 /****************************************************************************
- * Public Functions
+ * PRIVATE Functions
  ****************************************************************************/
-
+static void error_message(){
+  printf("VL53L1X help ussage:\n");
+  printf("vl53l1x -m <measure_mode> -c <calibration_distance>\n ");
+  printf("measure_mode:\n      short-> Up to 1.3 mtr and higher precision\n     long-> Up to 4 mtr and lower precision\n");
+  printf("calibration_distance:\n   Set a object to a known distance. Valid value between 10mm and 400mm or none\n ");
+}
 /****************************************************************************
  * hello_main
  ****************************************************************************/
@@ -57,23 +63,46 @@ int main(int argc, FAR char *argv[])
 int vl53l1x_main(int argc, char *argv[])
 #endif
 {
-  int fd; //HIH6130 file descriptor
-  uint16_t distancia;
-//struct hih6130_s sample;//Data structure of the sensor
+  uint16_t calib_value=0; //Variable to save the calibration value
+  int fd; //VL53L1X file descriptor
+  uint16_t result; // Variable to save the return data of the read.
 
-//Opening the sensor with read only permission
-fd=open("/dev/tof0", O_RDONLY);
-if(fd<0){
-  printf("Error opening HIH6130 sensor %d\n",fd_temp);
-}
-ioctl(fd,SNIOC_DISTANCESHORT);
-ioctl(fd,SNIOC_CALIBRATE,200);
-while(1){
-  //Read the data from the sensor and save in the structure
-  read(fd&distancia,sizeof(distancia));
-  printf("Distancia %i mm \r\n",distancia);
-  //Show the data and wait 1 second
-  //printf("Temperature: %d ºC Humidity: %d  \n",sample.temp,sample.hum);
-  usleep(100000);
-}
+  if(argc == 1 || (argc == 2) && !strcmp(argv[1],"-h") || argc > 5 || argc < 4)
+  {
+    error_message();// Just show the instructions of how to set-up this App
+    return 0;
+  }
+
+  //Opening the sensor with read only permission
+  fd=open("/dev/tof0", O_RDONLY);
+  // Setting the measure mode.
+  if(!strcmp(argv[2],"long")){
+    ioctl(fd,SNIOC_DISTANCELONG);
+  }
+  else if(!strcmp(argv[2],"short")){
+    ioctl(fd,SNIOC_DISTANCESHORT);
+  }
+  else{
+    error_message();// Just show the instructions of how to set-up this App
+    close(fd);
+    return 0;
+
+  }
+  //Set the calibration value
+  if(argc == 5){
+    calib_value = atoi(argv[4]);
+    if(calib_value > 10 || calib_value < 4000){
+      ioctl(fd,SNIOC_CALIBRATE,calib_value);
+    }
+    else{
+      printf("Error: Calibration not set, value out of range\r\n");
+    }
+  }
+
+  while(1){
+    // Read the distance every 100 mS.
+    read(fd,&result,sizeof(result));
+    printf("Distance %i mm \r\n",result);
+    usleep(100000);
+  }
 }

34596c9947b971d2ab2c94841713282935cd90f8
Alexandre M <amalki@piap.pl>
2020-08-26 08:35:15 +0000

    publisher/subscriber: follow the online examples

--- examples/publisher/publisher_main.c
+++ examples/publisher/publisher_main.c
@@ -2,7 +2,22 @@
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <std_msgs/msg/int32.h>
+#include <rmw_uros/options.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <nuttx/tracing/tracing_probes.h>
+
+#ifdef CONFIG_UROS_TRANSPORT_UDP
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <netinet/in.h>
+
+#include "netutils/netlib.h"
+#endif
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
 #if defined(BUILD_MODULE)
 int main(int argc, char *argv[])
@@ -10,53 +25,57 @@ int main(int argc, char *argv[])
 int publisher_main(int argc, char* argv[])
 #endif
 {
-    rcl_ret_t rv;
+    rcl_ret_t rv = RCL_RET_ERROR;
 
+#ifdef CONFIG_UROS_TRANSPORT_UDP
+    struct in_addr addr;
+    netlib_ifdown("eth0");
+    addr.s_addr = HTONL(0xc0a80a02);
+    netlib_set_ipv4addr("eth0", &addr);
+    netlib_ifup("eth0");
+    sleep(4);
+// wait to get the udpated 
+    system("ping -c1 192.168.10.1");
+#endif //
+    sys_trace_ctf_meas_start();
     rcl_init_options_t options = rcl_get_zero_initialized_init_options();
-    rv = rcl_init_options_init(&options, rcl_get_default_allocator());
-    if (RCL_RET_OK != rv) {
-        printf("rcl init options error: %s\n", rcl_get_error_string().str);
-        return 1;
-    }
+    RCCHECK(rcl_init_options_init(&options, rcl_get_default_allocator()))
 
     rcl_context_t context = rcl_get_zero_initialized_context();
-    rv = rcl_init(argc, argv, &options, &context);
-    if (RCL_RET_OK != rv) {
-        printf("rcl initialization error: %s\n", rcl_get_error_string().str);
-        return 1;
-    }
+    RCCHECK(rcl_init(argc, argv, &options, &context));
 
     rcl_node_options_t node_ops = rcl_node_get_default_options();
+
+    
+
     rcl_node_t node = rcl_get_zero_initialized_node();
-    rv = rcl_node_init(&node, "int32_publisher_rcl", "", &context, &node_ops);
-    if (RCL_RET_OK != rv) {
-        printf("Node initialization error: %s\n", rcl_get_error_string().str);
-        return 1;
-    }
+    RCCHECK(rcl_node_init(&node, "int32_publisher_rcl", "", &context, &node_ops));
 
     rcl_publisher_options_t publisher_ops = rcl_publisher_get_default_options();
     rcl_publisher_t publisher = rcl_get_zero_initialized_publisher();
-    rv = rcl_publisher_init(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "std_msgs_msg_Int32", &publisher_ops);
-    if (RCL_RET_OK != rv) {
-        printf("Publisher initialization error: %s\n", rcl_get_error_string().str);
-        return 1;
-    }
+//    publisher_ops.qos.reliability = RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
+
+    RCCHECK(rcl_publisher_init(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "std_msgs_msg_Int32", &publisher_ops));
 
     std_msgs__msg__Int32 msg;
-    const int num_msg = 1000;
+    const int num_msg = 50;
     msg.data = 0;
-    usleep(3000000); // As we are sending low number mensajes we need to wait discovery of the subscriber. (Do not have a notification on discovery)
+
+
+    sleep(2); // Sleep a while to ensure DDS matching before sending request
+    rcl_ret_t rc;
     do {
-        rv = rcl_publish(&publisher, (const void*)&msg, NULL);
-        if (RCL_RET_OK == rv )
-        {
-            printf("Sent: '%i'\n", msg.data++);
-        }
-    } while (RCL_RET_OK == rv && msg.data < num_msg );
+	    rc = rcl_publish(&publisher, (const void*)&msg, NULL);
+	    if (RCL_RET_OK == rc ) {
+		    printf("Sent: '%i'\n", msg.data++);
+	    }
+    } while (RCL_RET_OK == rc && msg.data < num_msg );
     printf("TOTAL sent: %i\n", num_msg);
 
-    rv = rcl_publisher_fini(&publisher, &node);
-    rv = rcl_node_fini(&node);
+    RCCHECK(rcl_publisher_fini(&publisher, &node));
+    RCCHECK(rcl_node_fini(&node));
+
+    sys_trace_ctf_meas_stop();
 
     return 0;
 }
--- examples/subscriber/subscriber_main.c
+++ examples/subscriber/subscriber_main.c
@@ -2,7 +2,22 @@
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <std_msgs/msg/int32.h>
+#include <rmw_uros/options.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <nuttx/tracing/tracing_probes.h>
+
+#ifdef CONFIG_UROS_TRANSPORT_UDP
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <netinet/in.h>
+
+#include "netutils/netlib.h"
+#endif
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
 #if defined(BUILD_MODULE)
 int main(int argc, char *argv[])
@@ -12,73 +27,54 @@ int subscriber_main(int argc, char* argv[])
 {
     rcl_ret_t rv = RCL_RET_ERROR;
 
-    rcl_init_options_t options = rcl_get_zero_initialized_init_options();
-    rv = rcl_init_options_init(&options, rcl_get_default_allocator());
-    if (RCL_RET_OK != rv) {
-        printf("rcl init options error: %s\n", rcl_get_error_string().str);
-        return 1;
-    }
+#ifdef CONFIG_UROS_TRANSPORT_UDP
+    struct in_addr addr;
+    netlib_ifdown("eth0");
+    addr.s_addr = HTONL(0xc0a80a03);
+    netlib_set_ipv4addr("eth0", &addr);
+    netlib_ifup("eth0");
+    system("ping -c2 192.168.10.1");
+#endif //
+  rcl_init_options_t options = rcl_get_zero_initialized_init_options();
+  RCCHECK(rcl_init_options_init(&options, rcl_get_default_allocator()));
 
-    rcl_context_t context = rcl_get_zero_initialized_context();
-    rv = rcl_init(argc, argv, &options, &context);
-    if (RCL_RET_OK != rv) {
-        printf("rcl initialization error: %s\n", rcl_get_error_string().str);
-        return 1;
-    }
+  rcl_context_t context = rcl_get_zero_initialized_context();
+  RCCHECK(rcl_init(argc, argv, &options, &context));
 
-    rcl_node_options_t node_ops = rcl_node_get_default_options();
-    rcl_node_t node = rcl_get_zero_initialized_node();
-    rv = rcl_node_init(&node, "int32_subscriber_rcl", "", &context, &node_ops);
-    if (RCL_RET_OK != rv)
-    {
-        fprintf(stderr, "[main] error in rcl : %s\n", rcutils_get_error_string().str);
-        rcl_reset_error();
-        return 1;
-    }
+  rcl_node_options_t node_ops = rcl_node_get_default_options();
+  rcl_node_t node = rcl_get_zero_initialized_node();
+  RCCHECK(rcl_node_init(&node, "int32_subscriber_rcl", "", &context, &node_ops));
 
-    rcl_subscription_options_t subscription_ops = rcl_subscription_get_default_options();
-    rcl_subscription_t subscription = rcl_get_zero_initialized_subscription();
-    rv = rcl_subscription_init(
-        &subscription, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "std_msgs_msg_Int32", &subscription_ops);
-    if (RCL_RET_OK != rv) {
-        printf("Subscription initialization error: %s\n", rcl_get_error_string().str);
-        return 1;
-    }
+  rcl_subscription_options_t subscription_ops = rcl_subscription_get_default_options();
+  rcl_subscription_t subscription = rcl_get_zero_initialized_subscription();
+  RCCHECK(rcl_subscription_init(&subscription, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "std_msgs_msg_Int32", &subscription_ops));
 
-    rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
-    rv = rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, &context, rcl_get_default_allocator());
-    if (RCL_RET_OK != rv) {
-        printf("Wait set initialization error: %s\n", rcl_get_error_string().str);
-        return 1;
-    }
+  rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
+  RCCHECK(rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, &context, rcl_get_default_allocator()));
+  sleep(2);
 
-    rv = rcl_wait_set_clear(&wait_set);
-    if (RCL_RET_OK != rv) {
-        printf("Wait set clear error: %s\n", rcl_get_error_string().str);
-        return 1;
-    }
-
+  std_msgs__msg__Int32 msg;
+  msg.data = 0;
+  do {
+    RCSOFTCHECK(rcl_wait_set_clear(&wait_set))
+    
     size_t index;
-    rv = rcl_wait_set_add_subscription(&wait_set, &subscription, &index);
-    if (RCL_RET_OK != rv) {
-        printf("Wait set add subscription error: %s\n", rcl_get_error_string().str);
-        return 1;
+    RCSOFTCHECK(rcl_wait_set_add_subscription(&wait_set, &subscription, &index));
+    
+    rcl_wait(&wait_set, 1000);
+     usleep(10000);
+    if (wait_set.subscriptions[index]) {
+      rcl_ret_t rc = rcl_take(wait_set.subscriptions[index], &msg, NULL, NULL);
+      if (RCL_RET_OK == rc) {
+        printf("I received: [%i]\n", msg.data);
+      }
     }
+  } while ( msg.data != 100 );
 
-    void* msg = rcl_get_default_allocator().zero_allocate(sizeof(std_msgs__msg__Int32), 1, rcl_get_default_allocator().state);
-    do {
-        rv = rcl_wait(&wait_set, 1000000);
-        for (size_t i = 0; i < wait_set.size_of_subscriptions; ++i) {
-        rv = rcl_take(wait_set.subscriptions[i], msg, NULL, NULL);
-        if (RCL_RET_OK == rv)
-        {
-            printf("I received: [%i]\n", ((const std_msgs__msg__Int32*)msg)->data);
-        }
-        }
-    } while ( RCL_RET_OK == rv || RCL_RET_SUBSCRIPTION_TAKE_FAILED == rv);
+  RCCHECK(rcl_subscription_fini(&subscription, &node));
+  RCCHECK(rcl_node_fini(&node));
 
-    rv = rcl_subscription_fini(&subscription, &node);
-    rv = rcl_node_fini(&node);
+  sys_trace_ctf_meas_stop();
 
-    return 0;
+  return 0;
 }

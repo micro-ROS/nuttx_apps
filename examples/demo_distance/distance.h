e9fad5bb63f2a6a6643c0fe7702831e797aca3a1
Andrzej Bratek <abratek@piap.pl>
2020-09-28 09:49:14 +0200

    Added  Demo Box apps

---
+++ examples/demo_distance/distance.h
@@ -0,0 +1,15 @@
+#ifndef __UCS_DISTANCE_DISTANCE_H__
+#define __UCS_DISTANCE_DISTANCE_H__
+
+
+
+#define DISTANCE_AGENT_INET_ADDR    "192.168.10.2"                  // micro-ROS agent IPv4 address
+#define DISTANCE_AGENT_UDP_PORT     "8888"                          // UDP port agent is listening on; below 10000
+#define DISTANCE_NODE               "polimexdst"                    // node name max 39 char
+#define DISTANCE_TOPIC              "distance_oli"                  // topic name max 39 char
+#define PUBLISHER_LOOP_DELAY_MS     180                             // loop delay in milliseconds
+#define WATCHDOG_TIME_SEC           5                               // watchdog expired time in seconds
+#define BOARD_STRING                "distance"                      // board string message
+
+#endif /* __UCS_DISTANCE_DISTANCE_H__ */
+

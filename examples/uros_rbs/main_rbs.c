afae8ab0141cf5d52be99785defc599864a73f39
Jan Staschulat <jan.staschulat@de.bosch.com>
2021-02-01 20:04:15 +0100

    baseline pongnode

--- examples/uros_rbs/main_rbs.c
+++ examples/uros_rbs/main_rbs.c
@@ -1,79 +1,88 @@
+// Copyright (c) 2020 Robert Bosch GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <pthread.h>
+
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
+#include <std_msgs/msg/int32.h>
 
-#include <std_msgs/msg/header.h>
-
-#include <stdio.h>
-#include <unistd.h>
-#include <time.h>
 
 #define STRING_BUFFER_LEN 100
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+#define RCUNUSED(fn) { rcl_ret_t temp_rc __attribute__((unused)); temp_rc = fn; }
 
-rcl_publisher_t ping_publisher;
-rcl_publisher_t pong_publisher;
-rcl_subscription_t ping_subscriber;
-rcl_subscription_t pong_subscriber;
 
-std_msgs__msg__Header incoming_ping;
-std_msgs__msg__Header outcoming_ping;
-std_msgs__msg__Header incoming_pong;
+rcl_subscription_t high_ping_subscription_;
+std_msgs__msg__Int32 high_ping_msg_;
+rcl_publisher_t high_pong_publisher_;
+rcl_subscription_t low_ping_subscription_;
+std_msgs__msg__Int32 low_ping_msg_;
+rcl_publisher_t low_pong_publisher_;
 
-int device_id;
-int seq_no;
-int pong_count;
 
-static
-void ping_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+void burn_cpu_cycles(long duration)
 {
-	(void) last_call_time;
-
-	if (timer != NULL) {
-
-		seq_no = rand();
-		sprintf(outcoming_ping.frame_id.data, "%d_%d", seq_no, device_id);
-		outcoming_ping.frame_id.size = strlen(outcoming_ping.frame_id.data);
-		
-		// Fill the message timestamp
-		struct timespec ts;
-		clock_gettime(CLOCK_REALTIME, &ts);
-		outcoming_ping.stamp.sec = ts.tv_sec;
-		outcoming_ping.stamp.nanosec = ts.tv_nsec;
-
-		// Reset the pong count and publish the ping message
-		pong_count = 0;
-		rcl_publish(&ping_publisher, (const void*)&outcoming_ping, NULL);
-		printf("Ping send seq %s\n", outcoming_ping.frame_id.data);  
-	}
+  if (duration > 0) {
+    clockid_t clockId;
+    clockId = CLOCK_REALTIME;
+    // clock_getcpuclockid(pthread_self(), &clockId);
+    // NuttX does not implement this function
+    // -lrt  is not available , I did not find this function in 
+    // NuttX subfolders
+    // adding EXTRA_LIBS to the Makefile did not resolve the error.
+    struct timespec startTimeP;
+    clock_gettime(clockId, &startTimeP);
+    int x = 0;
+    bool doAgain = true;
+    while (doAgain) {
+      while (x != rand() && x % 10 != 0) {
+        x++;
+      }
+      struct timespec currentTimeP;
+      clock_gettime(clockId, &currentTimeP);
+      long currentDuration = (currentTimeP.tv_sec - startTimeP.tv_sec) * 1000000000 + (currentTimeP.tv_nsec - startTimeP.tv_nsec);
+      doAgain = (currentDuration < duration);
+    }
+  }
 }
 
-static
-void ping_subscription_callback(const void * msgin)
-{
-	const std_msgs__msg__Header * msg = (const std_msgs__msg__Header *)msgin;
 
-	// Dont pong my own pings
-	if(strcmp(outcoming_ping.frame_id.data, msg->frame_id.data) != 0){
-		printf("Ping received with seq %s. Answering.\n", msg->frame_id.data);
-		rcl_publish(&pong_publisher, (const void*)msg, NULL);
-	}
+void high_ping_received(const void * pong_msg)
+{
+  burn_cpu_cycles(100000000);  // TODO: Get this value from parameter 'high_busyloop'.
+  printf("high ping received.\n");
+  RCUNUSED(rcl_publish(&high_pong_publisher_, pong_msg, NULL));
 }
 
-static
-void pong_subscription_callback(const void * msgin)
+
+void low_ping_received(const void * pong_msg)
 {
-	const std_msgs__msg__Header * msg = (const std_msgs__msg__Header *)msgin;
-
-	if(strcmp(outcoming_ping.frame_id.data, msg->frame_id.data) == 0) {
-			pong_count++;
-			printf("Pong for seq %s (%d)\n", msg->frame_id.data, pong_count);
-	}
+  burn_cpu_cycles(100000000);  // TODO: Get this value from parameter 'low_busyloop'.
+  printf("low ping received.\n");
+  RCUNUSED(rcl_publish(&low_pong_publisher_, pong_msg, NULL));
 }
-
 
 #if defined(BUILD_MODULE)
 int main(int argc, char *argv[])
@@ -81,65 +90,49 @@ int main(int argc, char *argv[])
 int uros_rbs_main(int argc, char* argv[])
 #endif
 {
-	rcl_allocator_t allocator = rcl_get_default_allocator();
-	rclc_support_t support;
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+  rclc_support_t support;
+  printf("Welcome to RBS Demo!\n");
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-	// create init_options
-	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  rcl_node_t node = rcl_get_zero_initialized_node();
+  RCCHECK(rclc_node_init_default(&node, "pong_rclc_node", "", &support));
 
-	// create node
-	rcl_node_t node = rcl_get_zero_initialized_node();
-	RCCHECK(rclc_node_init_default(&node, "pingpong_node", "", &support));
+  RCCHECK(rclc_publisher_init_best_effort(&high_pong_publisher_, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "high_pong"));
+  RCCHECK(rclc_subscription_init_best_effort(&high_ping_subscription_, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "high_ping"));
 
-	// Create a reliable ping publisher
-	RCCHECK(rclc_publisher_init_default(&ping_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/ping"));
+  RCCHECK(rclc_publisher_init_best_effort(&low_pong_publisher_, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "low_pong"));
+  RCCHECK(rclc_subscription_init_best_effort(&low_ping_subscription_, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "low_ping"));
 
-	// Create a best effort pong publisher
-	RCCHECK(rclc_publisher_init_best_effort(&pong_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/pong"));
+  rclc_executor_t high_executor = rclc_executor_get_zero_initialized_executor();
+  RCCHECK(rclc_executor_init(&high_executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_subscription(&high_executor, &high_ping_subscription_, &high_ping_msg_, &high_ping_received, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&high_executor, &low_ping_subscription_, &low_ping_msg_, &low_ping_received, ON_NEW_DATA));
+  // rclc_executor_t low_executor = rclc_executor_get_zero_initialized_executor();
+  // RCCHECK(rclc_executor_init(&low_executor, &support.context, 1, &allocator));
+  // RCCHECK(rclc_executor_add_subscription(&low_executor, &low_ping_subscription_, &low_ping_msg_, &low_ping_received, ON_NEW_DATA));
 
-	// Create a best effort ping subscriber
-	RCCHECK(rclc_subscription_init_best_effort(&ping_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/ping"));
+  rclc_executor_spin(&high_executor);
+  // TODO: And in second thread call: rclc_executor_spin(&low_executor);
 
-	// Create a best effort  pong subscriber
-	RCCHECK(rclc_subscription_init_best_effort(&pong_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/pong"));
+  RCCHECK(rcl_subscription_fini(&high_ping_subscription_, &node));
+  RCCHECK(rcl_publisher_fini(&high_pong_publisher_, &node));
+  RCCHECK(rcl_subscription_fini(&low_ping_subscription_, &node));  
+  RCCHECK(rcl_publisher_fini(&low_pong_publisher_, &node));
 
-
-	// Create a 3 seconds ping timer timer,
-	rcl_timer_t timer = rcl_get_zero_initialized_timer();
-	RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(2000), ping_timer_callback));
-
-
-	// Create executor
-	rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
-	RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
-
-	unsigned int rcl_wait_timeout = 1000;   // in ms
-	RCCHECK(rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout)));
-	RCCHECK(rclc_executor_add_timer(&executor, &timer));
-	RCCHECK(rclc_executor_add_subscription(&executor, &ping_subscriber, &incoming_ping, &ping_subscription_callback, ON_NEW_DATA));
-	RCCHECK(rclc_executor_add_subscription(&executor, &pong_subscriber, &incoming_pong, &pong_subscription_callback, ON_NEW_DATA));
-
-	// Create and allocate the pingpong messages
-
-	char outcoming_ping_buffer[STRING_BUFFER_LEN];
-	outcoming_ping.frame_id.data = outcoming_ping_buffer;
-	outcoming_ping.frame_id.capacity = STRING_BUFFER_LEN;
-
-	char incoming_ping_buffer[STRING_BUFFER_LEN];
-	incoming_ping.frame_id.data = incoming_ping_buffer;
-	incoming_ping.frame_id.capacity = STRING_BUFFER_LEN;
-
-	char incoming_pong_buffer[STRING_BUFFER_LEN];
-	incoming_pong.frame_id.data = incoming_pong_buffer;
-	incoming_pong.frame_id.capacity = STRING_BUFFER_LEN;
-
-	device_id = rand();
-
-	rclc_executor_spin(&executor);
-	
-	RCCHECK(rcl_publisher_fini(&ping_publisher, &node));
-	RCCHECK(rcl_publisher_fini(&pong_publisher, &node));
-	RCCHECK(rcl_subscription_fini(&ping_subscriber, &node));
-	RCCHECK(rcl_subscription_fini(&pong_subscriber, &node));
-	RCCHECK(rcl_node_fini(&node));
+  RCCHECK(rcl_node_fini(&node));
 }
+/*
+arm-none-eabi-ld --entry=__start -nostartfiles -nodefaultlibs -g -T/home/jst3si/olimex_ws/firmware/NuttX/configs/olimex-stm32-e407/scripts/ld.script -L"/home/jst3si/olimex_ws/firmware/NuttX/staging" -L"/home/jst3si/olimex_ws/firmware/NuttX/arch/arm/src/board" -L "/usr/lib/gcc/arm-none-eabi/6.3.1/../../../arm-none-eabi/lib/thumb/v7e-m/fpv4-sp/hard" \
+	-o "/home/jst3si/olimex_ws/firmware/NuttX/nuttx"   \
+	--start-group -lsched -ldrivers -lconfigs -lc -lmm -larch -lxx -lapps -lnet -lfs -lbinfmt -lxx -lboard -lsupc++ "/usr/lib/gcc/arm-none-eabi/6.3.1/thumb/v7e-m/fpv4-sp/hard/libgcc.a" --end-group
+/home/jst3si/olimex_ws/firmware/NuttX/staging/libapps.a(main_rbs.o): In function `burn_cpu_cycles':
+/home/jst3si/olimex_ws/firmware/apps/examples/uros_rbs/main_rbs.c:49: undefined reference to `clock_getcpuclockid'
+Makefile:184: recipe for target 'nuttx' failed
+make[1]: *** [nuttx] Error 1
+make[1]: Verzeichnis „/home/jst3si/olimex_ws/firmware/NuttX/arch/arm/src“ wird verlassen
+tools/Makefile.unix:422: recipe for target 'pass2' failed
+make: *** [pass2] Error 2
+
+
+*/
